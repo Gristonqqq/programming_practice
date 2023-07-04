b8bbdb2197c87a03812970df3eda91e60723b780
Oleksandr <sashatroan@gmail.com>
2023-07-04 19:15:59 +0300

    Task 5. Added comments

--- Task 5/main.c
+++ Task 5/main.c
@@ -5,14 +5,14 @@ int main() {
     int n, result, all,  badvariants = 0;
     printf("Введіть довжину послідовності:\n");
     scanf("%d", &n);
-    all = pow(2,n);
+    all = pow(2,n); //розрахунок всіх можливих варіантів послідовності
     do {
         for (int i = 0; i < n - 2; i++) {
             badvariants += n - (2 + i);
         }
         n-=4;
-    } while (n > 2);
-    result = all - badvariants;
+    } while (n > 2); //цикл, який розраховує всі "погані" для нас варіанти(де три одиниці потрапляють підряд)
+    result = all - badvariants; //віднімаємо від усіх варіантів "погані"
     printf("Кількість послідовностей: %d", result);
     return 0;
 }
